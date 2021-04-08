581edac5cc041527c905929a758ca3af1863c5c3
Sharon-07 <sharonhernandez07@gmail.com>
2021-04-08 00:20:08 -0600

    Referencia a la pagina principal

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Ejemplos en <a href="https://nextjs.org">Matlab</a>
+          Ejemplos en <a href="https://hammingolay.vercel.app/">Matlab</a>
         </h1>
 
         <div className={styles.grid}>
